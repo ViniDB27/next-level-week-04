104be03c1854075f55bd295fef6cf02d65395904
vinicios deivid brejinski <viniciosdb@gmail.com>
2021-02-26 10:36:22 -0300

    time 25 min

--- moveit-next/src/contexts/CountDownContext.tsx
+++ moveit-next/src/contexts/CountDownContext.tsx
@@ -25,7 +25,7 @@ export function CountDownProvider({ children }: CountDownProviderProvider){
     const { startNewChallange } = useContext(ChallangesContext)
     
     const [isActive, setIsActive] = useState(false)
-    const [time, setTime] = useState(0.1 * 60)
+    const [time, setTime] = useState(25 * 60)
     const [ hasFinished, setHasFinished ] = useState(false)
     
     const minutes = Math.floor(time / 60);
